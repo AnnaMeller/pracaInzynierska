b2c1baa73232ca3d0fdd51a15464c5923f887eb2
Anna Meller <ameller@localhost.localdomain>
2017-10-15 00:58:38 +0200

    IntensityLed work with value from Android, but not correct

--- PracaInzynierska/PythonScript/intesityLed.py
+++ PracaInzynierska/PythonScript/intesityLed.py
@@ -10,17 +10,11 @@ GPIO.setup (22, GPIO.OUT)
 pwm_led = GPIO.PWM(22, 50)
 pwm_led.start(0)
 
-try:
-	while True:
-		duty_s = sys.argv[1]
-		duty = int(duty_s)
-		pwm_led.ChangeDutyCycle(duty)
-		time.sleep(0.5)
+duty_s = sys.argv[1]
+duty = int(duty_s)
+pwm_led.ChangeDutyCycle(duty)
+time.sleep(0.5)
 
-except KeyboardInterrupt:
-	print "Exiting Program"
-except:
-	print "Error Occurs, Exiting Program"
-finally:
-	GPIO.cleanup()
-print sys.argv[0]
+time.sleep(10)
+#GPIO.output(22,GPIO.HIGH)
+#GPIO.cleanup()
