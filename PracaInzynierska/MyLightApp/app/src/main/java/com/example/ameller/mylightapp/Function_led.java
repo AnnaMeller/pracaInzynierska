cc18d0985532d367ec6c749327b4c0584a57a00a
Anna Meller <ameller@localhost.localdomain>
2017-10-29 20:16:51 +0100

    Changing intensity of led RGB via AndroidApp working good

--- PracaInzynierska/MyLightApp/app/src/main/java/com/example/ameller/mylightapp/Function_led.java
+++ PracaInzynierska/MyLightApp/app/src/main/java/com/example/ameller/mylightapp/Function_led.java
@@ -7,15 +7,11 @@ package com.example.ameller.mylightapp;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.flask.colorpicker.ColorPickerView;
-import com.flask.colorpicker.OnColorChangedListener;
-import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 import com.kosalgeek.asynctask.AsyncResponse;
 import com.kosalgeek.asynctask.PostResponseAsyncTask;
 
@@ -37,9 +33,13 @@ public class Function_led extends AppCompatActivity implements View.OnClickListe
 
     Button onButtonLed;
     Button offButtonLed;
-    SeekBar brightnessBarLed;
-    TextView valueViewLed;
-    TextView valueViewColor;
+    SeekBar seekBarRed;
+    TextView valueViewProgessRed;
+    SeekBar seekBarGreen;
+    TextView valueViewProgessGreen;
+    SeekBar seekBarBlue;
+    TextView valueViewProgressBlue;
+
 
     //private ArrayBlockingQueue<Integer> mQueue = new ArrayBlockingQueue<Integer>(100);
 
@@ -53,25 +53,117 @@ public class Function_led extends AppCompatActivity implements View.OnClickListe
         offButtonLed = (Button) findViewById(R.id.offButtonLed);
         offButtonLed.setOnClickListener(this);
 
-        brightnessBarLed = (SeekBar) findViewById(R.id.brightnessBarLed);
-        valueViewLed = (TextView) findViewById(R.id.valueViewLed);
-        valueViewColor = (TextView) findViewById(R.id.valueViewColor);
+        seekBarRed = (SeekBar) findViewById(R.id.seekBarRed);
+        valueViewProgessRed = (TextView) findViewById(R.id.textViewProgressRed);
+        seekBarGreen = (SeekBar) findViewById(R.id.seekBarGreen);
+        valueViewProgessGreen = (TextView) findViewById(R.id.textViewProgressGreen);
+        seekBarBlue = (SeekBar) findViewById(R.id.seekBarBlue);
+        valueViewProgressBlue = (TextView) findViewById(R.id.textViewProgressBlue);
 
 
-        brightnessBarLed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
             public void onProgressChanged(SeekBar brightnessBar, int progress, boolean fromUser) {
-                valueViewLed.setText("" + progress);
+
+                valueViewProgessRed.setText("" + progress);
 
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
 
-                        String value = valueViewLed.getText().toString();
+                        String value = valueViewProgessRed.getText().toString();
 
                         HttpClient httpclient = new DefaultHttpClient();
-                        HttpPost httpPost = new HttpPost("http://192.168.1.103:8090/led.php");
+                        HttpPost httpPost = new HttpPost("http://192.168.1.103:8090/led_red.php");
+
+                        try {
+                            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                            nameValuePairs.add(new BasicNameValuePair("value", value));
+                            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                            //Execute HTTP Post Request
+                            HttpResponse response = httpclient.execute(httpPost);
+                        } catch (ClientProtocolException e) {
+
+                        } catch (IOException e) {
+
+                        }
+                    }
+                }).start();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        seekBarGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar brightnessBar, int progress, boolean fromUser) {
+
+                valueViewProgessGreen.setText("" + progress);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String value = valueViewProgessGreen.getText().toString();
+
+                        HttpClient httpclient = new DefaultHttpClient();
+                        HttpPost httpPost = new HttpPost("http://192.168.1.103:8090/led_green.php");
+
+                        try {
+                            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                            nameValuePairs.add(new BasicNameValuePair("value", value));
+                            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                            //Execute HTTP Post Request
+                            HttpResponse response = httpclient.execute(httpPost);
+                        } catch (ClientProtocolException e) {
+
+                        } catch (IOException e) {
+
+                        }
+                    }
+                }).start();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        seekBarBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar brightnessBar, int progress, boolean fromUser) {
+
+                valueViewProgressBlue.setText("" + progress);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String value = valueViewProgressBlue.getText().toString();
+
+                        HttpClient httpclient = new DefaultHttpClient();
+                        HttpPost httpPost = new HttpPost("http://192.168.1.103:8090/led_blue.php");
 
                         try {
                             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
@@ -120,23 +212,8 @@ public class Function_led extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
-            ColorPickerDialogBuilder
-                .with(context)
-                .setTitle("Choose color")
-                .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
-                .density(12)
-                .setOnColorChangedListener(new OnColorChangedListener() {
 
-
-                    @Override
-                    public void onColorChanged(int selectedColor) {
-                        // Handle on color change
-                        Log.d("ColorPicker", "onColorChanged: 0x" + Integer.toHexString(selectedColor));
-                        valueViewColor.setText("" + Log.d("ColorPicker", "onColorChanged: 0x" + Integer.toHexString(selectedColor)));
-                    }
-                });
     }
-
 
     @Override
     public void processFinish(String s) {
