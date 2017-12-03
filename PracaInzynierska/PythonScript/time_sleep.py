b4d8dad53925bc68191b184e714783a219c1b8e8
Anna Meller <ameller@localhost.localdomain>
2017-12-03 18:14:34 +0100

    Python Script turn on or off light after set hour

---
+++ PracaInzynierska/PythonScript/time_sleep.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+
+import time
+import RPi.GPIO as GPIO
+import sys
+
+time_to_off = sys.argv[1]
+#s = time.strptime(time_to_off, "%I:%M")
+
+hour_now = time.strftime("%H")
+print (hour_now)
+minute_now = time.strftime("%M")
+if minute_now[:1] == 0:
+    minute_now1 = minute_now[1:]
+else:
+    minute_now1 = minute_now
+
+len = len(time_to_off)
+
+if time_to_off[2] == ':':
+
+	hour_next = int(time_to_off[:2])
+	minute_next = int(time_to_off[3:])
+else:
+	hour_next = int(time_to_off[:1])
+	minute_next = int(time_to_off[2:])
+
+hour = hour_next - int(hour_now)
+print (hour)
+minute = minute_next - int(minute_now1)
+
+sleep_time = hour*3600+minute*60
+print (sleep_time)
+time.sleep(sleep_time)
+print sleep_time
+
+#pi = pigpio.pi()
+
+if GPIO.input == True:
+
+	#turnOn relay
+	GPIO.setmode(GPIO.BCM)
+	GPIO.cleanup()
+	GPIO.setwarnings(False)
+	GPIO.setup(25, GPIO.OUT)
+	GPIO.output(25, GPIO.LOW)
+
+else:
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.cleanup()
+	GPIO.setwarnings(False)
+	GPIO.setup(25, GPIO.OUT)
+	GPIO.output(25, GPIO.HIGH)
+
+#pi.stop() #disconnect with Pi
