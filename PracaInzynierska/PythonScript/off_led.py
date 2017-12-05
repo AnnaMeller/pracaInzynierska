c71aeb08f8f8c6cf2e3137fbb1ec2c2fe04a58ce
Anna Meller <ameller@localhost.localdomain>
2017-12-05 23:43:46 +0100

    Off_led script with kill led_strip process

--- PracaInzynierska/PythonScript/off_led.py
+++ PracaInzynierska/PythonScript/off_led.py
@@ -2,15 +2,20 @@
 import RPi.GPIO as GPIO
 import time
 import pigpio
+import sys
+import os
+
 pi = pigpio.pi()
+GPIO.cleanup()
 
 RedPin = 17
 GreenPin = 22
 BluePin = 24
 
+
 #redOff
 pi.set_PWM_dutycycle(RedPin, 0)
-
+	
 #greenOff
 pi.set_PWM_dutycycle(GreenPin, 0)
 
@@ -23,4 +28,9 @@ GPIO.cleanup()
 GPIO.setwarnings(False)
 GPIO.setup(23, GPIO.OUT)
 GPIO.output(23, GPIO.HIGH)
+
+GPIO.cleanup();
+
+os.system(" ps aux  |  grep -i python  |  awk '{print $2}'  |  xargs sudo kill -9")
+
 
